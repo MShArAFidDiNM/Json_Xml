5c5aed4e3597f7ecf15c2dd8ae3288ba57a49005
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-06-21 00:58:37 +0500

    JsonTask-1

--- Json_Xml/Json_Xml/Program.cs
+++ Json_Xml/Json_Xml/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace Json_Xml
+﻿using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace Json_Xml
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("[1] - JSON           [2] - XML");
+            int input =  int.Parse(Console.ReadLine());
+            if (input == 1 )
+            {
+                Json();
+            }
+        }
+        public static void Json()
+        {
+            Employe employee = new Employe
+            {
+                EmployeName = "Employee"
+            };
+
+            var department = new Department
+            {
+                DepartmentName = "Department",
+                EmployeName = "Employe"
+            };
+
+            string path = "C:\\Tekshir\\Json\\1.json";
+            if (File.Exists(path))
+            {
+                    string text =  File.ReadAllText(path);
+                    var desirilazer = JsonSerializer.Deserialize<Department>(text);
+                    Console.WriteLine(desirilazer);
+               
+            }
+            else
+            { 
+                File.Create(path).Close();
+                string jsonString = JsonSerializer.Serialize(department);
+                Console.WriteLine("Created");
+
+                File.WriteAllText(path, jsonString);
+            }                   
         }
     }
 }
